b2a0ebbfaf72c8d59046121d1d43b41a49c2f8e4
Vincent Wong <vincent@vwong.dev>
2020-07-18 23:32:33 +1000

    Remove duplicate files

--- map.hs
+++
@@ -1,5 +0,0 @@
-import Prelude hiding (map)
-
-map :: (a -> b) -> [a] -> [b]
-map f [] = []
-map f (x:xs) = f x : map f xs
--- zip.hs
+++
@@ -1,6 +0,0 @@
-import Prelude hiding (zip)
-
-zip :: (a -> b -> c) -> [a] -> [b] -> [c]
-zip f [] _ = []
-zip f _ [] = []
-zip f (x:xs) (y:ys) = (x `f` y) : zip f xs ys
